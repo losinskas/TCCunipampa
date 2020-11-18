4b1a3ef182e37ea632ce6630bdf6e38418230878
lucas losinskas <lucas_losinskas@hotmail.com>
2020-11-18 07:12:28 -0300

    18/11/2020

--- Software_JAVA/src/Main.java
+++ Software_JAVA/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 //  Método principal para os inputs de dados iniciais
     public static void main(String[] args) {
         double Q, Q_min, H, H_max, h_sum, Z_b, eta_v, eta_m;
-/*
+
         Q = 15;         // Vazão de entrada [m³/s]
         Q_min = 10;     // Vazão mínima [m³/s]
         H = 60;         // Altura de operação [m]
@@ -11,7 +11,7 @@ public class Main {
         Z_b = 200;      // Altura em relação ao nível do mar [m]
         eta_v = 0.99;   // Rendimento volumétrico do rotor
         eta_m = 0.97;   // Remdimento mecânico do conjunto Q = 15;         // Vazão de entrada [m³/s]
-        */
+        /*
         Q = 3.6;
         Q_min = 10;     // Vazão mínima [m³/s]
         H = 21.35;         // Altura de operação [m]
@@ -20,7 +20,7 @@ public class Main {
         Z_b = 500;      // Altura em relação ao nível do mar [m]
         eta_v = 0.96;   // Rendimento volumétrico do rotor
         eta_m = 0.91;   // Remdimento mecânico do conjunto
-        /*
+
         Q = 3.8;         // Vazão de entrada [m³/s]
         Q_min = 2;     // Vazão mínima [m³/s]
         H = 21.7;         // Altura de operação [m]
